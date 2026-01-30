1b997cd5f5038f316fea097cd204d244ef227427
Karan <karanlokchandani@protonmail.com>
2026-01-30 11:39:58 +0530

    feat: antrea capture k8s controller
    Signed-off-by: Karan <karanlokchandani@protonmail.com>

---
+++ antrea-capture/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/user/antrea-capture/pkg/controller"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+)
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	nodeName := os.Getenv("NODE_NAME")
+	if nodeName == "" {
+		klog.Fatal("NODE_NAME environment variable is required")
+	}
+
+	klog.Infof("Starting packet capture controller on node: %s", nodeName)
+
+	config, err := buildConfig()
+	if err != nil {
+		klog.Fatalf("Failed to build k8s config: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Failed to create k8s client: %v", err)
+	}
+
+	ctrl := controller.New(clientset, nodeName)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigCh
+		klog.Infof("Received signal: %v", sig)
+		cancel()
+	}()
+
+	if err := ctrl.Run(ctx); err != nil {
+		klog.Fatalf("Controller error: %v", err)
+	}
+
+	klog.Info("Controller stopped gracefully")
+}
+
+func buildConfig() (*rest.Config, error) {
+	if _, err := os.Stat("/var/run/secrets/kubernetes.io/serviceaccount/token"); err == nil {
+		klog.Info("Running in-cluster, using service account")
+		return rest.InClusterConfig()
+	}
+
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if kubeconfig == "" {
+		kubeconfig = os.Getenv("HOME") + "/.kube/config"
+	}
+
+	if _, err := os.Stat(kubeconfig); err == nil {
+		klog.Infof("Using kubeconfig: %s", kubeconfig)
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+
+	return nil, fmt.Errorf("unable to find kubeconfig or in-cluster config")
+}
---
+++ antrea-capture/pkg/controller/controller.go
@@ -0,0 +1,253 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog/v2"
+)
+
+const (
+	annotationKey = "tcpdump.antrea.io"
+)
+
+type Controller struct {
+	clientset kubernetes.Interface
+	nodeName  string
+	captures  map[string]*exec.Cmd
+	mu        sync.RWMutex
+	stopCh    chan struct{}
+}
+
+func New(clientset kubernetes.Interface, nodeName string) *Controller {
+	return &Controller{
+		clientset: clientset,
+		nodeName:  nodeName,
+		captures:  make(map[string]*exec.Cmd),
+		stopCh:    make(chan struct{}),
+	}
+}
+
+func (c *Controller) Run(ctx context.Context) error {
+	klog.Infof("Starting controller for node: %s", c.nodeName)
+
+	fieldSelector := fields.OneTermEqualSelector("spec.nodeName", c.nodeName).String()
+	factory := informers.NewSharedInformerFactoryWithOptions(
+		c.clientset,
+		time.Minute,
+		informers.WithTweakListOptions(func(options *metav1.ListOptions) {
+			options.FieldSelector = fieldSelector
+		}),
+	)
+
+	podInformer := factory.Core().V1().Pods().Informer()
+
+	if _, err := podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    c.handlePodAdd,
+		UpdateFunc: c.handlePodUpdate,
+		DeleteFunc: c.handlePodDelete,
+	}); err != nil {
+		return fmt.Errorf("failed to add event handler: %w", err)
+	}
+
+	factory.Start(c.stopCh)
+
+	if !cache.WaitForCacheSync(ctx.Done(), podInformer.HasSynced) {
+		return fmt.Errorf("failed to sync caches")
+	}
+
+	klog.Info("Controller synced and ready")
+
+	<-ctx.Done()
+	return c.Shutdown()
+}
+
+func (c *Controller) Shutdown() error {
+	klog.Info("Shutting down controller, stopping all captures")
+	close(c.stopCh)
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for podName, cmd := range c.captures {
+		klog.Infof("Stopping capture for pod: %s", podName)
+		c.stopCaptureInternal(podName, cmd)
+	}
+
+	return nil
+}
+
+func (c *Controller) handlePodAdd(obj interface{}) {
+	pod := obj.(*corev1.Pod)
+	klog.V(4).Infof("Pod added: %s/%s", pod.Namespace, pod.Name)
+	c.processPod(pod)
+}
+
+func (c *Controller) handlePodUpdate(oldObj, newObj interface{}) {
+	oldPod := oldObj.(*corev1.Pod)
+	newPod := newObj.(*corev1.Pod)
+
+	oldAnnotation := oldPod.Annotations[annotationKey]
+	newAnnotation := newPod.Annotations[annotationKey]
+
+	if oldAnnotation != newAnnotation {
+		klog.Infof("Pod %s/%s annotation changed from '%s' to '%s'",
+			newPod.Namespace, newPod.Name, oldAnnotation, newAnnotation)
+		c.processPod(newPod)
+	}
+}
+
+func (c *Controller) handlePodDelete(obj interface{}) {
+	pod := obj.(*corev1.Pod)
+	klog.Infof("Pod deleted: %s/%s", pod.Namespace, pod.Name)
+	c.stopCapture(pod.Name)
+}
+
+func (c *Controller) processPod(pod *corev1.Pod) {
+	podKey := pod.Name
+	annotation := pod.Annotations[annotationKey]
+
+	if annotation == "" {
+		c.stopCapture(podKey)
+		return
+	}
+
+	maxFiles, err := strconv.Atoi(annotation)
+	if err != nil || maxFiles <= 0 {
+		klog.Errorf("Invalid annotation value '%s' for pod %s: %v", annotation, podKey, err)
+		c.stopCapture(podKey)
+		return
+	}
+
+	c.mu.RLock()
+	_, exists := c.captures[podKey]
+	c.mu.RUnlock()
+
+	if exists {
+		klog.V(4).Infof("Capture already running for pod %s, skipping", podKey)
+		return
+	}
+
+	c.startCapture(podKey, maxFiles)
+}
+
+func (c *Controller) startCapture(podName string, maxFiles int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, exists := c.captures[podName]; exists {
+		return
+	}
+
+	captureFile := fmt.Sprintf("/capture-%s.pcap", podName)
+	klog.Infof("Starting capture for pod %s (max files: %d, output: %s)",
+		podName, maxFiles, captureFile)
+
+	cmd := exec.Command("tcpdump",
+		"-C", "1M",
+		"-W", strconv.Itoa(maxFiles),
+		"-w", captureFile,
+		"-i", "any",
+		"-n",
+	)
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+
+	if err := cmd.Start(); err != nil {
+		klog.Errorf("Failed to start tcpdump for pod %s: %v", podName, err)
+		return
+	}
+
+	c.captures[podName] = cmd
+
+	go func() {
+		if err := cmd.Wait(); err != nil {
+			klog.V(4).Infof("tcpdump for pod %s exited: %v", podName, err)
+		}
+
+		c.mu.Lock()
+		delete(c.captures, podName)
+		c.mu.Unlock()
+	}()
+}
+
+func (c *Controller) stopCapture(podName string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	cmd, exists := c.captures[podName]
+	if !exists {
+		return
+	}
+
+	c.stopCaptureInternal(podName, cmd)
+}
+
+func (c *Controller) stopCaptureInternal(podName string, cmd *exec.Cmd) {
+	klog.Infof("Stopping capture for pod %s", podName)
+
+	if cmd.Process != nil {
+		klog.V(4).Infof("Sending SIGTERM to tcpdump process for pod %s", podName)
+		if err := syscall.Kill(-cmd.Process.Pid, syscall.SIGTERM); err != nil {
+			klog.V(4).Infof("Failed to send SIGTERM to tcpdump process for pod %s: %v", podName, err)
+		}
+
+		done := make(chan error, 1)
+		go func() {
+			done <- cmd.Wait()
+		}()
+
+		select {
+		case <-done:
+			klog.V(4).Infof("tcpdump for pod %s terminated gracefully", podName)
+		case <-time.After(5 * time.Second):
+			klog.Warningf("tcpdump for pod %s did not terminate gracefully, killing", podName)
+			if err := syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL); err != nil {
+				klog.V(4).Infof("Failed to send SIGKILL to tcpdump process for pod %s: %v", podName, err)
+			}
+		}
+	}
+
+	delete(c.captures, podName)
+
+	c.cleanupFiles(podName)
+}
+
+func (c *Controller) cleanupFiles(podName string) {
+	pattern := fmt.Sprintf("/capture-%s.pcap*", podName)
+	klog.Infof("Cleaning up capture files matching: %s", pattern)
+
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		klog.Errorf("Failed to glob pattern %s: %v", pattern, err)
+		return
+	}
+
+	for _, file := range matches {
+		klog.V(4).Infof("Deleting capture file: %s", file)
+		if err := os.Remove(file); err != nil {
+			klog.Errorf("Failed to delete file %s: %v", file, err)
+		}
+	}
+
+	if len(matches) == 0 {
+		klog.V(4).Infof("No capture files found for pod %s", podName)
+	} else {
+		klog.Infof("Deleted %d capture file(s) for pod %s", len(matches), podName)
+	}
+}
